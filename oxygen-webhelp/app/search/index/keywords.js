dc090fce2ebc781fa754d5feb554d0771e914b13
Mervin <mervin.george@outlook.com>
2024-02-08 23:08:16 +0530

    Commit Documentation Build
    Commit Documentation Build #08022024

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,9 +1,10 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Generic",p:["p1"]},{w:"PDF",p:["p1"]},{w:"Links",p:["p1"]},{w:"Documentation",p:["p2"]},{w:"Impacts",p:["p2"]},{w:"Maintenace/Hot",p:["p3"]},{w:"Fix",p:["p3"]},{w:"Release",p:["p3"]},{w:"Notes",p:["p3"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Generic",p:["p1"]},{w:"PDF",p:["p1"]},{w:"Links",p:["p1"]},{w:"Documentation",p:["p2"]},{w:"Impacts",p:["p2"]},{w:"Logistics",p:["p3"]},{w:"and",p:["p3"]},{w:"CarrierGo",p:["p3"]},{w:"Feature",p:["p3"]},{w:"Configurations",p:["p3"]},{w:"Maintenace/Hot",p:["p4"]},{w:"Fix",p:["p4"]},{w:"Release",p:["p4"]},{w:"Notes",p:["p4"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3, 4, 5];
 ph["p2"]=[6, 7];
-ph["p3"]=[8, 9, 10, 11];
+ph["p3"]=[8, 9, 10, 11, 12];
+ph["p4"]=[13, 14, 15, 16];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
