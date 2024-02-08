3368065ad1058ede3028db97f19cf7a15779f23c
Mervin <mervin.george@outlook.com>
2024-02-08 19:37:27 +0530

    Commit Documentation Build
    Commit Documentation Build #08022024

--- oxygen-webhelp/app/context-help/context-help-map.js
+++ oxygen-webhelp/app/context-help/context-help-map.js
@@ -1,1 +1,1 @@
-var contextHelpMap = [{"appname":"", "appid":"PDFLinks", "path":"oxy_ex-1/TopicTree/PDFLinks.html"},{"appname":"", "appid":"RNPDFLinks", "path":"oxy_ex-1/TopicTree/RNPDFLinks.html"}];
+var contextHelpMap = [{"appname":"", "appid":"PDFLinks", "path":"oxy_ex-1/TopicTree/PDFLinks.html"},{"appname":"", "appid":"RNPDFLinks", "path":"oxy_ex-1/TopicTree/RNPDFLinks.html"},{"appname":"", "appid":"GenericPDFLinks", "path":"oxy_ex-1/TopicTree/GenericPDFLinks.html"}];
--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,8 +1,9 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Documentation",p:["p1"]},{w:"Impacts",p:["p1"]},{w:"Maintenace/Hot",p:["p2"]},{w:"Fix",p:["p2"]},{w:"Release",p:["p2"]},{w:"Notes",p:["p2"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Generic",p:["p1"]},{w:"PDF",p:["p1"]},{w:"Links",p:["p1"]},{w:"Documentation",p:["p2"]},{w:"Impacts",p:["p2"]},{w:"Maintenace/Hot",p:["p3"]},{w:"Fix",p:["p3"]},{w:"Release",p:["p3"]},{w:"Notes",p:["p3"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
-ph["p1"]=[3, 4];
-ph["p2"]=[5, 6, 7, 8];
+ph["p1"]=[3, 4, 5];
+ph["p2"]=[6, 7];
+ph["p3"]=[8, 9, 10, 11];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
