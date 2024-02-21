a4dcf8dfe5019a738e06398717f451b586c94105
Mervin <mervin.george@outlook.com>
2024-02-21 11:08:09 +0530

    Commit Documentation Build #21022024

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,12 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"API",p:["p1"]},{w:"PDF",p:["p1","p2"]},{w:"Links",p:["p1","p2"]},{w:"Generic",p:["p2"]},{w:"Documentation",p:["p3"]},{w:"Impacts",p:["p3"]},{w:"Logistics",p:["p4"]},{w:"and",p:["p4"]},{w:"CarrierGo",p:["p4"]},{w:"Feature",p:["p4"]},{w:"Configurations",p:["p4"]},{w:"Maintenace/Hot",p:["p5"]},{w:"Fix",p:["p5"]},{w:"Release",p:["p5"]},{w:"Notes",p:["p5"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5];
+ph["p2"]=[6, 4, 5];
+ph["p3"]=[7, 8];
+ph["p4"]=[9, 10, 11, 12, 13];
+ph["p5"]=[14, 15, 16, 17];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
