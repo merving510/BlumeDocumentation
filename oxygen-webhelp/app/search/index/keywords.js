3300ac2f59a7ed41b938f38ae63d488ab1470684
Mervin <mervin.george@wisetechglobal.com>
2024-02-25 21:32:19 +0530

    Commit Documentation Build
    Commit Documentation Build #25022024

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,13 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"API",p:["p1"]},{w:"PDF",p:["p1","p3"]},{w:"Links",p:["p1","p3"]},{w:"Asset",p:["p2"]},{w:"Management",p:["p2"]},{w:"Generic",p:["p3"]},{w:"Documentation",p:["p4"]},{w:"Impacts",p:["p4"]},{w:"Logistics",p:["p5"]},{w:"and",p:["p5"]},{w:"CarrierGo",p:["p5"]},{w:"Feature",p:["p5"]},{w:"Configurations",p:["p5"]},{w:"Maintenace/Hot",p:["p6"]},{w:"Fix",p:["p6"]},{w:"Release",p:["p6"]},{w:"Notes",p:["p6"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5];
+ph["p2"]=[6, 7];
+ph["p3"]=[8, 4, 5];
+ph["p4"]=[9, 10];
+ph["p5"]=[11, 12, 13, 14, 15];
+ph["p6"]=[16, 17, 18, 19];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
